98b23beb4c90f02c9660a71c9112babb118ce54e
IAmLeEpic <te15.jonathan.sjogren@umea.nti.se>
2017-10-10 15:14:27 +0200

    Databas, formulär funktion
    La till formulär funktion i bloggen, med databas skit å allt

--- index.php
+++ index.php
@@ -10,14 +10,33 @@
 <body>
 	<div class="blogContainer">
 		<div class="blogContent">
-			<?php 
-			include_once("data.php");
 
-			foreach ($data as $key => $value) {
-				echo "<article><h1> {$value["title"]}</h1>";
-				echo "<p>{$value["message"]}</p>";
-				echo "<p>\"Written By\" {$value["author"]}</p>";
-				echo "</article>";	
+			<form action="index.php" method="post">
+				Title:
+				<input type="text" name="title">
+				Message:
+				<input type="text" name="message">
+				<input type="submit" name="submit">
+			</form>
+
+			<?php 
+				if (isset($_POST['submit'])) {
+					$dbh = new PDO("mysql:host=localhost;dbname=post;charset=utf8", "root", "");
+
+					$sql = "insert into posts(userid, title, content)
+							values (1, '" . $_POST['title'] . "', '" . $_POST['message']. "')";
+					$stmt = $dbh->prepare($sql);
+					$stmt->execute();
+				}
+
+			$sql = "select * from posts"; 
+			$stmt = $dbh->prepare($sql); 
+			$stmt->execute();
+			
+			while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+				//echo "<pre>" . print_r($row,1) . "</pre>";
+				echo "<h1> {$row['title']} </h1>";
+				echo "<p> {$row['content']} </p>";
 			}
 
 			?>
